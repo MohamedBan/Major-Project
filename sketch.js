0f6788bc00f7f870997c19b6ac2b46afae9b5b0b
Banigesh4910151 <4910151@sps-student.sk.ca>
2022-11-17 12:13:22 -0600

    Already begun, this is what I have so far

--- sketch.js
+++ sketch.js
@@ -6,10 +6,157 @@
 // - describe what you did to take this project "above and beyond"
 
 
+const ROWS = 40;
+const COLS = 40;
+let grid;
+let cellWidth;
+let cellHeight;
+let playerX = 0;
+let playerY = 0;
+let stoneImg;
+let grassImg;
+let survivorImg;
+let knifeGif;
+let reloadGif;
+let bullets;
+let muzzle;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellWidth = width/COLS;
+  cellHeight = height/ROWS;
+  grid = createRandom2dArray(COLS, ROWS);
+  //place player in grid
+  grid[playerY][playerX] = 9;
+}
+
+function preload(){
+  stoneImg = loadImage("rock.png");
+  grassImg = loadImage("grass.png");
+  survivorImg = loadImage("survivor.png");
+  knifeGif = loadImage("knife.gif");
+  reloadGif = loadImage("reload.gif");
+  bullets = loadImage("bullet.png");
+  muzzle = loadImage("muzzle.png");
 }
 
 function draw() {
   background(220);
+  displayGrid(grid);
+}
+
+function keyPressed() {
+  if (keyCode === " "){
+    knifeGif.play();
+  }
+  if (keyCode === RIGHT_ARROW) {
+    if (grid[playerY][playerX+1] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerX++;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === LEFT_ARROW) {
+    if (grid[playerY][playerX-1] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerX--;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === UP_ARROW) {
+    if (grid[playerY-1][playerX] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerY--;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === DOWN_ARROW) {
+    if (grid[playerY+1][playerX] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerY++;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+}
+
+function mousePressed() {
+  let xPos = Math.floor(mouseX/cellWidth);
+  let yPos = Math.floor(mouseY/cellHeight);
+
+  if (grid[yPos][xPos] === 0) {
+    grid[yPos][xPos] = 1;
+  }
+  else if (grid[yPos][xPos] === 1) {
+    grid[yPos][xPos] = 0;
+  }
+}
+
+function displayGrid(grid) {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
+      if (grid[y][x] === 0) {
+        image(grassImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+        
+      }
+      else if (grid[y][x] === 1) {
+        image(stoneImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+      else if (grid[y][x] === 9) {
+        image(grassImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+        image(survivorImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+        
+      }
+    }
+  }
+}
+
+function create2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y=0; y<ROWS; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<COLS; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandom2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y=0; y<ROWS; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<COLS; x++) {
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
 }
