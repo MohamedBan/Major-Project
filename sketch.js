2dbbc63b252300ec4fcbc05d6e490905af91eb8f
Banigesh4910151 <4910151@sps-student.sk.ca>
2022-11-21 12:19:15 -0600

    added new file

--- sketch.js
+++ sketch.js
@@ -6,30 +6,6 @@
 // - describe what you did to take this project "above and beyond"
 
 
-class Bullet {
-  constructor(bulletArr, character, img){
-    this.character = character;
-    this.x = this.character.x + this.character.width;
-    this.y = this.character.y + this.character.height/2;
-    this.img = img;
-    this.bulletArr = bulletArr;
-  }
-
-  display(){
-    image(this.img, this.x, this.y, this.img.width*0.1, this.img.height*0.1);
-  }
-
-  update(){
-    this.x += 5;
-
-    if (this.x > windowWidth){
-      this.bulletArr.splice(this.bulletArr.indexOf(this), 1);
-    }
-  }
-
-}
-
-
 const ROWS = 30;
 const COLS = 30;
 let grid;
@@ -90,6 +66,7 @@ function draw() {
 
 function keyPressed() {
   if (keyCode === " "){
+    // eslint-disable-next-line no-undef
     bulletArr.push(new Bullet(bulletArr, rectangle, bulletImg));
   }
   if (keyCode === "g"){
